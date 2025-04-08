83cdb22d12d053441f2dadea80cf7bb915a34c49
Bohdan Shraier <bohdanshraier@Bohdans-MacBook-Air.local>
2025-04-08 21:43:05 +0300

    added health endpoint

---
+++ src/main/java/com/example/knifes/controllers/AppController.java
@@ -0,0 +1,33 @@
+package com.example.knifes.controllers;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("app")
+public class AppController {
+
+
+    @GetMapping("/health")
+    public ResponseEntity<String> ping() throws JsonProcessingException {
+        try {
+            Map<String,String> responseBody = new HashMap<>();
+            responseBody.put("Message", "success");
+            return  ResponseEntity.ok(new ObjectMapper().writeValueAsString(responseBody));
+        } catch (JsonProcessingException e) {
+            return  ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+
+    }
+}
